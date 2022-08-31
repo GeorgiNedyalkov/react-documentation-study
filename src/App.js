2b715d9cdd57fdd2d5e87468e234f61b0b676146
Flu3nt <gnedyalkov94@gmail.com>
2022-08-31 14:59:23 +0300

    Create a Essay Form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css"
 import React from "react"
 import NameForm from "./components/NameForm"
+import EssayForm from "./components/EssayForm"
+import FlavorForm from "./components/FlavorForm"
 
 function App() {
   return (
     <div className="App">
       <NameForm />
+      <EssayForm />
+      <FlavorForm />
     </div>
   )
 }
